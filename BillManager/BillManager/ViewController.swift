3e7bc9f9c905dda9cbc937b7db9fb8a197f95e6a
Andrew Hwang <andrewhwang@Andrews-MBP.fios-router.home>
2017-11-12 16:58:09 -0500

    Created the project

---
+++ BillManager/BillManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BillManager
+//
+//  Created by Andrew Hwang on 11/12/17.
+//  Copyright © 2017 Andrew and Tony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
